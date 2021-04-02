0f00f1b19c8038e6206d27c2c22701a1508056c8
Rafael Buttignon <rafaaugustocontato@gmail.com>
2021-04-01 22:32:00 -0300

    Pagina de login

---
+++ AluraPicStudy/src/app/home/home.module.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VMessageComponent } from '../shared/components/vmessage/vmessage.component';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignInComponent } from './signin/signin.component';
+
+
+@NgModule({
+    declarations: [ 
+        SignInComponent,
+     ],
+     imports: [ 
+         CommonModule,
+         ReactiveFormsModule,
+         VMessageModule 
+    ],
+})
+export class HomeModule { }
