0c33db953b4dba0887cc8919d5abcecf1ff2f498
Rafael Buttignon <rafaaugustocontato@gmail.com>
2021-04-03 18:25:26 -0300

    Validações de cadastro

--- AluraPicStudy/src/app/home/home.module.ts
+++ AluraPicStudy/src/app/home/home.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule }  from '@angular/forms';
+import { FormsModule, ReactiveFormsModule }  from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/singup.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ 
+        SignInComponent,
+        SignUpComponent
+    ],
     imports: [ 
         CommonModule, 
         ReactiveFormsModule,
         VMessageModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ]
 })
 export class HomeModule { }
